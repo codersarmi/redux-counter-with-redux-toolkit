86b935f17f45f8c99b234536a1a2426a9f682be7
codersarmi <kaung.woody@gmail.com>
2023-07-30 22:04:13 +0630

    redux-counter updated with redux toolkit

---
+++ src/components/Counter.jsx
@@ -0,0 +1,44 @@
+import "./Counter.css";
+import { useSelector, useDispatch } from "react-redux";
+import { counterAction } from "../Store/index";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  const isShow = useSelector((state) => state.isShow);
+  const increaseHlandler = () => {
+    dispatch(counterAction.increase());
+  };
+  const decreaseHandler = () => {
+    dispatch(counterAction.decrease());
+  };
+  const increaseBy5 = () => {
+    dispatch(counterAction.increaseBy5(5));
+  };
+  const toggleHandler = () => {
+    dispatch(counterAction.toggle());
+  };
+  return (
+    <section className="counter">
+      <h1>Redux Counter</h1>
+      {isShow && <h2>{counter}</h2>}
+      <hr />
+      <div className="counter_btns">
+        <button className="counter-btn" onClick={increaseHlandler}>
+          Increase
+        </button>
+        <button className="counter-btn" onClick={increaseBy5}>
+          Increaseby5
+        </button>
+        <button className="counter-btn" onClick={decreaseHandler}>
+          Decrease
+        </button>
+        <button className="counter-btn" onClick={toggleHandler}>
+          Toggle
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Counter;
